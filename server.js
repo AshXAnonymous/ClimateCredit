308c6bf87b98f590a85f89f0bfed749eb6f1f3eb
AshXAnonymous <vermaansh30122005@gmail.com>
2026-03-21 22:38:05 +0530

    server

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.use("/api", contactRoutes);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
+    console.log("MONGO URI:", process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
     app.listen(process.env.PORT || 8080, () =>
       console.log(`🚀 Server running on port ${process.env.PORT || 8080}`)
