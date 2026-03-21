129b318c764638b1b176236376486c1682a315ba
AshXAnonymous <vermaansh30122005@gmail.com>
2026-03-21 20:39:19 +0530

    Cors

--- server.js
+++ server.js
@@ -22,15 +22,9 @@ const contactRoutes = require("./routes/contactRoutes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://climate-credit-trwo.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "*"
+}));
 
 
 app.get("/api/health", (req, res) => {
