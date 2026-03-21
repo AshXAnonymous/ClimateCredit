9ddfcde75331b593d11bf3e57703e8243093b810
Ansh Verma <vermaansh30122005@gmail.com>
2026-03-21 21:00:17 +0530

    Remove trailing slash from CORS origin URL

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "https://climatecredit.vercel.app/",
+  origin: "https://climatecredit.vercel.app",
 }));
 
 
